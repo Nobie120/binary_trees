f93aa3645c611b280d1f6f528133ebbf8b1feb68
Nobie120 <n12507400@gmail.com>
2024-01-04 15:09:54 -0800

    adding a node on the left of the prent

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,6 +22,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new->left = parent->left;
 		parent->left->parent = new;
 	}
+	else
+		parent->left = new;
 
 	return (new);
 }
